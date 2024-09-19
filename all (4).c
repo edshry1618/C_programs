dd7b1532dbfc1773abae87205824369162816a0c
Shaurya <edshaurya@gmail.com>
2024-09-19 09:38:46 +0530

    Add files via upload

---
+++ all (4).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	for(int i=1;i<=5;i++){
+	    for(int k=1;k<=i;k++){
+	        printf("*");
+	    }
+	    printf("\n");
+	}
+	
+	return 0;
+
+}
+
