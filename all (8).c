12191393138e48bf700493e02c0397cc0a38228b
Shaurya <edshaurya@gmail.com>
2024-09-24 09:51:00 +0530

    Add files via upload

---
+++ all (8).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+	    for(int j=i;j<=n-1;j++)
+	        printf(" ");
+	    for(int k=1;k<=2*(i-1)-1;k++)
+	        printf("*");
+	    printf("\n");
+	}
+
+}
