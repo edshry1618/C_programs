5f7cbecf93008489da7177a7080ac2af57beee59
Shaurya <edshaurya@gmail.com>
2024-12-16 05:38:47 +0530

    Update Palindrome_string.c

--- Palindrome_string.c
+++ Palindrome_string.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
-	// your code goes here
-	char word[50];
-	int flag=0;
-	printf("Enter the word:");
-	scanf("%s",word);
-	int len=strlen(word);
-	for(int i=0;i<len;i++){
-	    if(word[i]!=word[len-i-1]){
-	        flag=1;
-	        break;
-	    }
-	}
-	if(flag==1)
-	    printf("NO");
-	else 
-	    printf("YES");
+    char str[100], rev[100];
+    int length, i, flag = 1;
 
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Convert the string to lowercase for case-insensitive comparison
+    for (i = 0; str[i] != '\0'; i++) {
+        str[i] = tolower(str[i]);
+    }
+
+    length = strlen(str);
+
+    // Reversing the string
+    for (i = 0; i < length; i++) {
+        rev[i] = str[length - i - 1];
+    }
+    rev[length] = '\0'; // Null-terminate the reversed string
+
+    // Checking if the original string is equal to its reverse
+    for (i = 0; i < length; i++) {
+        if (str[i] != rev[i]) {
+            flag = 0; // Not a palindrome
+            break;
+        }
+    }
+
+    if (flag) {
+        printf("The string \"%s\" is a palindrome.\n", str);
+    } else {
+        printf("The string \"%s\" is not a palindrome.\n", str);
+    }
+
+    return 0;
 }
-
